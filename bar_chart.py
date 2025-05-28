09b6db91883a392cc3e26df393335f9fe4172108
dertannbach <der.tannbach@gmail.com>
2025-05-28 15:59:16 +0800

    bar_chart.py

--- bar_chart.py
+++ bar_chart.py
@@ -1,6 +1,7 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
+# 多列堆叠柱状图
 tick_label = ["A", "B", "C", "D"]
 y = [67, 94, 46, 17]   # left, train
 y1 = [48, 67, 37, 13]  # right, train
@@ -18,21 +19,21 @@ x = np.arange(4)
 bar_width = 0.3  # 条形宽度
 c1 = '#B3CDE0'
 c2 = '#F1A7C1'
-c3 = '#C1D9A0'
+c3 = '#C1D9A0' # 颜色设置
 plt.bar(x, y, bar_width, color=c1, label='x1')
-plt.bar(x, y1, bar_width, bottom=y,  # 堆叠在第一个上方
+plt.bar(x, y1, bar_width, bottom=y,  # 堆叠
         color=c2, label='x2', alpha=0.5)
 plt.bar(x, y2, bar_width, bottom=[sum(x) for x in zip(y, y1)],
         color=c3, label='x3', alpha=0.5)
 
 plt.bar(x + bar_width, y3, bar_width,  align="center", color=c1)
-plt.bar(x + bar_width, y4, bar_width, bottom=y3,  # 堆叠在第一个上方
+plt.bar(x + bar_width, y4, bar_width, bottom=y3,  # 堆叠
         color=c2, alpha=0.5)
 plt.bar(x + bar_width, y5, bar_width, bottom=[sum(x) for x in zip(y3, y4)],
         color=c3, alpha=0.5)
 
 plt.bar(x + 2 * bar_width, y6, bar_width,  align="center", color=c1)
-plt.bar(x + 2 * bar_width, y7, bar_width, bottom=y6,  # 堆叠在第一个上方
+plt.bar(x + 2 * bar_width, y7, bar_width, bottom=y6,  # 堆叠
         color=c2, alpha=0.5)
 plt.bar(x + 2 * bar_width, y8, bar_width, bottom=[sum(x) for x in zip(y6, y7)],
         color=c3, alpha=0.5)
